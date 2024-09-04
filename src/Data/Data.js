4951fb1c7806243cb7a77ff0f07fddfd6a10aa79
Rachel-Cellulose <99023474+Rachel-Cellulose@users.noreply.github.com>
2024-09-04 10:22:25 +0100

    first commit for sidebar

---
+++ src/Data/Data.js
@@ -0,0 +1,36 @@
+import {
+     UilUser,
+    UilMoneyWithdrawal,  
+  UilHistory,         
+  UilExchangeAlt,  
+  UilSignOutAlt,    
+  UilSetting           
+} from '@iconscout/react-unicons';
+
+export const SidebarData = [
+  {
+    icon: UilUser,
+    heading: "Profile",
+  },
+  {
+    icon: UilMoneyWithdrawal,
+    heading: "Reward",
+  },
+  {
+    icon: UilHistory,
+    heading: "History",
+  },
+  {
+    icon: UilExchangeAlt,
+    heading: "Cashout",
+  },
+
+  {
+    icon: UilSetting,
+    heading: "Setting",
+  },
+  {
+    icon: UilSignOutAlt,
+    heading: "SignOut",
+  },
+];
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import "./Sidebar.scss";
+import { SidebarData } from "../Data/Data";
+import logo from "../images/balanceè_logo.jpeg";
+
+
+const Sidebar = () => {
+
+    const [selected, setSelected] = useState(0)
+  return (
+    <div className="sidebar">
+      <div className="side_logo">
+        <img src={logo} alt="" />
+      </div>
+
+      {/* menubar */}
+
+      <div className="menu_bar">
+        {SidebarData.map((item, index) => {
+          return (
+            <div className={selected===index? "menu_item active" : "menu_item" }
+            key={index}
+            onClick={() =>setSelected(index)}
+            >
+
+              <item.icon />
+              <span>{item.heading}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
